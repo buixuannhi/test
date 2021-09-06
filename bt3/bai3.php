09f6aea047e5deff62f710056836ca512adbac68
bui nhi <buixuannhi@gmail.com>
2021-09-06 16:18:24 +0700

    ok

---
+++ bt3/bai3.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+if(isset($_SESSION['counter'])){
+    $_SESSION['counter'] += 1;
+}else{
+    $_SESSION['counter']=1;
+}
+$msg = "bạn đã truy cập trang này  ". $_SESSION['counter'];
+$msg .="  lần trong session này .";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>thiết lập session trong php </title>
+</head>
+<body>
+    <?php echo $msg ?>
+</body>
+</html>
